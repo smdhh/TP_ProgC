65694a6ac3b14ccf13d54a1f1bf183b123216e50
sami diah <Sami.diah@icloud.com>
2025-11-21 10:32:07 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -4,8 +4,9 @@
 int main() {
 int a = 16;
 int b = 3;  
-    printf("A + B = " , a + b);
-   printf("B - A = " , b - a);
-   printf("A x B = " , a * b);
-   printf("A / B = " , a / b);
+    printf("A + B = " , a+b);
+   printf("B - A = " , b-a);
+   printf("A x B = " , a*b);
+   printf("A / B = " , a/b);
 }
+
