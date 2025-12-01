10a517ce63363a30b1a10567be7620859e77f576
sami diah <Sami.diah@icloud.com>
2025-12-01 15:39:11 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 5; 
+    int compteur = 5;  // doit être < 10
 
     for (int i = 1; i <= compteur; i++) {
-
         for (int j = 1; j <= i; j++) {
-            if (j % 2 == 0)
-                printf("# ");
-            else
-                printf("* ");
-        }
 
+            // 1) Première et dernière colonne → toujours *
+            if (j == 1 || j == i)
+                printf("* ");
+
+            // 2) Dernière ligne → que des *
+            else if (i == compteur)
+                printf("* ");
+
+            // 3) Sinon → intérieur en #
+            else
+                printf("# ");
+        }
         printf("\n");
     }
 
