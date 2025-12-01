feac00a609edc1476f339a76b67e76281c695e4a
sami diah <Sami.diah@icloud.com>
2025-12-01 15:44:56 +0100

    Update conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int somme = 0;
+
+    for (int i = 1; i <= 1000; i++) {
+
+        if (i % 11 == 0) {
+            continue; // On saute les multiples de 11
+        }
+
+        if (i % 5 == 0 || i % 7 == 0) {
+            somme += i;
+        }
+
+        if (somme > 5000) {
+            break; // On arrête immédiatement
+        }
+    }
+
+    printf("Somme finale = %d\n", somme);
+
+    return 0;
+}
